b404bb578c27027d1724b448b96466e1c6e948ce
joao <joao182cx@gmail.com>
2019-01-21 10:03:43 -0200

    Adição dos botões que controlarão o jogo.

--- src/CrapsFrame.java
+++ src/CrapsFrame.java
@@ -1,4 +1,53 @@
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
-public class CrapsFrame {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class CrapsFrame extends JFrame {
+	
+	private final JPanel panelBotoes;
+	private final JPanel panelInformacoes;	
+	
+	private final JButton botaoJogarDados;
+	private final JButton botaoIniciar;
+	private final JButton botaoJogarNovamente;
+	
+	/*
+	private final JLabel labelDadoUm;
+	private final JLabel labelDadoDois;
+	private final JLabel labelSoma;
+	private final JLabel labelPontuacao;
+	
+	private final JTextField textoDadoUm;
+	private final JTextField textoDadoDois;
+	private final JTextField textoSoma;
+	private final JTextField textolPontuacao;
+	*/
+	
+	public CrapsFrame() {
+		
+		super("Jogo de Craps");
+		setLayout(new GridLayout(2, 1));
+		
+		panelBotoes = new JPanel();
+		panelBotoes.setLayout(new FlowLayout());
+		add(panelBotoes);
+		
+		panelInformacoes = new JPanel();
+		panelInformacoes.setLayout(new GridLayout(4, 2));
+		add(panelInformacoes);
+		
+		botaoJogarDados = new JButton("Jogar Dados!");
+		botaoIniciar = new JButton("Iniciar!");
+		botaoJogarNovamente = new JButton("Jogar Novamente!");
+		
+		panelBotoes.add(botaoIniciar);
+		panelBotoes.add(botaoJogarDados);
+		panelBotoes.add(botaoJogarNovamente);
+	}
 
 }
---
+++ src/CrapsTeste.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class CrapsTeste {
+
+	public static void main(String[] args) {
+		
+		CrapsFrame craps = new CrapsFrame();
+		craps.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		craps.setSize(200, 200);
+		craps.setVisible(true);
+		
+	}
+
+}
